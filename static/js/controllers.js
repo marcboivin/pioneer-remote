049fd48b4d61d514c24107a99316ae08c75ae9a7
Marc Boivin <marc.boivin@libeo.com>
2015-08-08 08:51:05 -0400

    Now load all the commands from JSON file devices.json more features to come

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,22 +1,20 @@
 var pioneerApp = angular.module('pioneerRemoteApp', []);
 
-pioneerApp.controller('ButtonListCtrl', ['$scope', '$http', function ($scope) {
+pioneerApp.controller('ButtonListCtrl', ['$scope', '$http', function ($scope, $http) {
 
-	$http.get('devices.json')
+	$scope.buttons = []
+
+	$http.get('/static/js/devices.json')
         .success(function(data) {
         		console.log(data)
+        		$scope.buttons = data.PIONEERSC1228.commands
         })
         .error(function(data,status,error,config){
         	console.log(data)
         	console.log(status)
         	console.log(error)
         });
-  $scope.buttons = [
-    {'name': 'Volume UP',
-     'action': 'VU'},
-     {'name': 'Volume Down',
-   		'action': 'VD'}
-  ];
+        
   $scope.send_cmd = function(cmd){
   	jQuery.get('run/'+ cmd)
   }
