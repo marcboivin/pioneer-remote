ad87cfec78c40eedc001d0f8d673db4c9567cc9e
Marc Boivin <marc.boivin@libeo.com>
2015-08-08 08:49:40 -0400

    Minimal exception support. send_cmd will sent False when failing

--- remote.py
+++ remote.py
@@ -6,19 +6,23 @@ class Remote:
 		self.ip = IP
 
 	def send_cmd(self, cmd):
-		#Assume UTF-8 data, could be dedection based	
-		udata=cmd.decode("utf-8")
-		asciidata=udata.encode("ascii","ignore")
+		try:
+			#Assume UTF-8 data, could be dedection based	
+			udata=cmd.decode("utf-8")
+			asciidata=udata.encode("ascii","ignore")
 
-		tn = telnetlib.Telnet(self.ip, 24)
+			tn = telnetlib.Telnet(self.ip, 24)
 
-		r = tn.read_until("$ ", 0.1)
+			r = tn.read_until("$ ", 0.1)
 
-		#assert "$ " in r, "Timeout waiting for prompt!"
+			#assert "$ " in r, "Timeout waiting for prompt!"
 
-		tn.write(asciidata + "\r\n")
+			tn.write(asciidata + "\r\n")
 
-		tn.close()
+			tn.close()
+			return True
+		except:
+			return False
 
 	def connect_to_celery_queue(self, params):
 		return True
