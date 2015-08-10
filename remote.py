3f647bb2a2a6fa16d7f8f109f68b615cbbfe0e66
Marc Boivin <marc.boivin@libeo.com>
2015-08-10 09:05:10 -0400

    Command validation so we do not send arbitrary input to the telnet server

--- remote.py
+++ remote.py
@@ -1,28 +1,45 @@
 import telnetlib
+import json
 
 
 class Remote:
-	def __init__ (self, IP):
+	def __init__ (self, IP, commands_json):
+		self.json_commands = commands_json
 		self.ip = IP
+		self.commands = False
+		self.get_allowed_commands()
 
 	def send_cmd(self, cmd):
 		try:
-			#Assume UTF-8 data, could be dedection based	
-			udata=cmd.decode("utf-8")
-			asciidata=udata.encode("ascii","ignore")
+			# Valdation the action against the allow action list
+			for action in self.commands:
+				if action["action"]== cmd :
 
-			tn = telnetlib.Telnet(self.ip, 24)
+					# Converting to ASCII caus PIONEERR's telnet is from 1904
+					udata=cmd.decode("utf-8")
+					asciidata=udata.encode("ascii","ignore")
 
-			r = tn.read_until("$ ", 0.1)
+					tn = telnetlib.Telnet(self.ip, 24)
 
-			#assert "$ " in r, "Timeout waiting for prompt!"
+					r = tn.read_until("$ ", 0.1)
 
-			tn.write(asciidata + "\r\n")
+					#assert "$ " in r, "Timeout waiting for prompt!"
 
-			tn.close()
-			return True
+					tn.write(asciidata + "\r\n")
+
+					tn.close()
+
+					return True
+
+			return False
+
 		except:
 			return False
 
-	def connect_to_celery_queue(self, params):
-		return True
+	def get_allowed_commands(self):
+		json_data=open(self.json_commands).read()
+		data = json.loads(json_data)
+		# Hardcoded value, should be passed as parmeter.
+		# Will be the next feture
+		self.commands = data["PIONEERSC1228"]["commands"]
+
